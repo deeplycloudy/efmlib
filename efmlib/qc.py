0c297d0af8867ac647d96569e9010970e701507b
michael.stock <michael.stock@mstock.winstorm.nssl>
2023-02-13 09:58:16 -0600

    Updates to io.df_fiber_qc to work a bit better, I hope

--- efmlib/qc.py
+++ efmlib/qc.py
@@ -2,19 +2,43 @@ import numpy as np
 import pandas as pd
 
 def df_fiber_qc(df_fiber, data_period=22.5, max_milli=2):
-    # Remove any dts that seem unreal
-    df_fiber['dt'] = df_fiber.diff()['adc_ready_millis']
-    df_fiber['dt'] = np.abs(data_period - df_fiber['dt'])
-    # print (np.sum(df_fiber['dt'] > max_milli))
-    df_fiber = df_fiber[df_fiber['dt'] < max_milli] # max millisec difference from expectation
+    """
+    Data recorded by the EFM has many many artifacts in the output.  
+    These are likely due to the crap connection on the fiber at the rotating 
+    interface.  The fiber here is just a butt connection, and one side of it 
+    is rotating.  Not something that exactly meets the quality standards of 
+    Norman building code.  So, we need to go through the data pretty carefully 
+    looking for crap that isn't real.
+
+    This will mean that later on, we don't get monotonically spaced time.  
+    Hope no one needs to do any Fourier analysis
+    """
+
+    # first to examine is time.  This field is stored in the obviously named
+    # 'adc_ready_millis' field
+    # what unit is millis exactly Eric?  Why do they need to be ready?  Who knows
+
+    # The most consistent way I've found to filter time is to calculate the 
+    # average period, and make sure it's within expected bounds
+    # we can't check for it to be over a lower bound, because the data recording 
+    # has a tendency to restart part way through the flight
+    df_fiber['period'] = df_fiber['adc_ready_millis']/np.arange( len(df_fiber['adc_ready_millis']) )
+    df_fiber = df_fiber[ (df_fiber['period']<data_period)]
+
 
     # extra filter: get rid of repeated, lines
     fields = ['acceleration_x', 'acceleration_y', 'acceleration_z',
               'magnetometer_x', 'magnetometer_y', 'magnetometer_z',
               'gyroscope_x', 'gyroscope_y', 'gyroscope_z']
-
     df_fiber = df_fiber[(df_fiber.diff()[fields] == 0).sum(axis=1)<6]
-    df_fiber = df_fiber[np.abs((df_fiber.diff()['adc_ready_millis']))<1e4]
+
+    # get rid of lines where the adc reading is out of range
+    # the EFM voltage is read off of a 24 bit adc, then read into a 32 bit field
+    # so there's a lot of room for bunk values
+    # the test is against 2**23 because the value is signed
+    df_fiber = df_fiber[np.abs(df_fiber['adc_reading'])<2**23]  
+
+    # df_fiber = df_fiber[np.abs((df_fiber.diff()['adc_ready_millis']))<1e4]
     return df_fiber
 
 def fix_adc_offset(df, dt=0.065, up=50, adc_var='adc_volts_withlag'):
