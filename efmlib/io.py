ca9aeb7958ca747d5232911a588e3be6395b654d
michael.stock <michael.stock@mstock.winstorm.nssl>
2024-01-04 10:36:07 -0600

    modified packets_convert to not loop over already found packets

--- efmlib/io.py
+++ efmlib/io.py
@@ -55,74 +55,104 @@ def decode_data_packet_old(mp):
     result['end_byte'] = struct.unpack('B', mp[51:])[0]
     return result
 
-def packets_convert(ba, return_all=False,
-                    gps_packet_length=20, gps_start_bytes=[],
-                    data_packet_length=34, data_start_bytes=[],
+def packets_convert(ba, 
                     shift_dt=0.0):
+
+    gps_packet_length=20
+    gps_start_bytes=[]
+    data_packet_length=34
+    data_start_bytes=[]
+
     # Find Starts
-    for i in range(len(ba) - gps_packet_length):
+    # re-written to loop only once, and not loop through the packets
+    # we've already found
+    i = 0
+    while i < len(ba) - max( gps_packet_length, data_packet_length ):
         if (ba[i] == 254) and (ba[i+gps_packet_length-1] == 237):
             gps_start_bytes.append(i)
-
-    # Process GPS
-    gps_raw_packets = []
-    bytes_to_remove = []
-    for sb in gps_start_bytes:
-        gps_raw_packets.append(ba[sb:sb+gps_packet_length])
-        bytes_to_remove.append(range(sb, sb+gps_packet_length))
-    if len(gps_raw_packets) == 0:
-        gps_raw_packets.append(bytearray(gps_packet_length))
-
-    bytes_to_remove = np.array(bytes_to_remove)
-    bytes_to_remove = bytes_to_remove.ravel()
-    #ba = np.delete(ba, bytes_to_remove)
-
-    gps_packets = []
-    for p in gps_raw_packets:
-        gps_packets.append(decode_gps_packet(p))
-
-
-    series = {}
-    for field in gps_packets[0].keys():
-        vals = []
-        for p in gps_packets:
-            vals.append(p[field])
-        series[field] = vals
-    df_gps = pd.DataFrame(series)
-    df_gps['gps_time'] =  pd.to_datetime(df_gps['gps_time'], format='%H%M%S00', errors='coerce')
-    df_gps.replace([0], np.nan, inplace=True)
+            i += gps_packet_length
+        elif (ba[i] == 190) and (ba[i+data_packet_length-1] == 239):
+            data_start_bytes.append(i)
+            i += data_packet_length
+        else:
+            i += 1
+    # for i in range(len(ba) - gps_packet_length):
+    #     if (ba[i] == 254) and (ba[i+gps_packet_length-1] == 237):
+    #         gps_start_bytes.append(i)
 
     # Find Starts
-    for i in range(len(ba) - data_packet_length):
-        if (ba[i] == 190) and (ba[i+data_packet_length-1] == 239):
-            data_start_bytes.append(i)
+    # for i in range(len(ba) - data_packet_length):
+    #     if (ba[i] == 190) and (ba[i+data_packet_length-1] == 239):
+    #         data_start_bytes.append(i)
 
-    # Process
-    data_raw_packets = []
+    # # Process GPS
+    # gps_raw_packets = []
+    # bytes_to_remove = []
+    # for sb in gps_start_bytes:
+    #     gps_raw_packets.append(ba[sb:sb+gps_packet_length])
+    #     bytes_to_remove.append(range(sb, sb+gps_packet_length))
+    # if len(gps_raw_packets) == 0:
+    #     gps_raw_packets.append(bytearray(gps_packet_length))
+
+    ##
+    # convert all the GPS data into a dict of field names
+    gps_series = {}
+    for sb in gps_start_bytes:
+        
+        decoded_gps_packet = decode_gps_packet( ba[sb:sb+gps_packet_length] )
+        for k in decoded_gps_packet:
+            if not k in gps_series:
+                gps_series[k] = []
+            gps_series[k].append( decoded_gps_packet[k] )
+
+    # And now we can make the panda dataframe
+    if len( gps_series ) > 0:
+        df_gps = pd.DataFrame(gps_series)
+        df_gps['gps_time'] =  pd.to_datetime(df_gps['gps_time'], format='%H%M%S00', errors='coerce')
+        df_gps.replace([0], np.nan, inplace=True)
+    else:
+        df_gps = None
+
+
+    # do the same thing to the data packets
+    # this is pretty much duplicated code
+    ##
+    # convert all the GPS data into a dict of field names
+    data_series = {}
     for sb in data_start_bytes:
-        data_raw_packets.append(ba[sb:sb+data_packet_length])
+        
+        decoded_data_packet = decode_data_packet( ba[sb:sb+data_packet_length] )
+        for k in decoded_data_packet:
+            if not k in data_series:
+                data_series[k] = []
+            data_series[k].append( decoded_data_packet[k] )
 
-    data_packets = []
-    for p in data_raw_packets:
-        data_packets.append(decode_data_packet(p))
+    # # Process
+    # data_raw_packets = []
+    # for sb in data_start_bytes:
+    #     data_raw_packets.append(ba[sb:sb+data_packet_length])
 
-    series = {}
-    for field in data_packets[0].keys():
-        vals = []
-        for p in data_packets:
-            vals.append(p[field])
-        series[field] = vals
-    df_fiber = pd.DataFrame(series)
+    # data_packets = []
+    # for p in data_raw_packets:
+    #     data_packets.append(decode_data_packet(p))
+
+    # series = {}
+    # for field in data_packets[0].keys():
+    #     vals = []
+    #     for p in data_packets:
+    #         vals.append(p[field])
+    #     series[field] = vals
+    df_fiber = pd.DataFrame(data_series)
 
     adc_cal = (2 * 2.048) / 2**24
     adc_cal *= 2 # For voltage divider that is in-place
     df_fiber['adc_volts_withlag'] = adc_cal * df_fiber['adc_reading']
     df_fiber['adc_volts'] = fix_adc_offset(df_fiber, dt=shift_dt)
 
-    if return_all==True:
-        return df_gps, df_fiber, data_packets, gps_packets
-    else:
-        return df_gps, df_fiber
+    # if return_all==True:
+    #     return df_gps, df_fiber, data_packets, gps_packets
+    # else:
+    return df_gps, df_fiber
 
 def read_efm_raw(filenames, shift_dt=0.065):
     """ Read raw EFM files from SD card given a list of filenames.
